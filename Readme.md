6ab29377d07fb4589eb0ec1a975e638bda5efacf
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-06 13:53:34 +0200

    Update usage info

--- Readme.md
+++ Readme.md
@@ -69,11 +69,8 @@ Usage
 
   EXAMPLES
 
-    # Rename a file:
+    # Rename a file or directory:
     $ mve Readdme.md Readme.md
-
-    # Move a directory:
-    $ mve --mkdirp source/directory target/directory
 
     # Read from a file, then pipe into it:
     $ format-json package.json > .temp; mve --force .temp package.json
--- module/bin/help/examples.js
+++ module/bin/help/examples.js
@@ -3,11 +3,8 @@ const {bold} = require('chalk');
 module.exports =
 `  ${bold('EXAMPLES')}
 
-    # Rename a file:
+    # Rename a file or directory:
     $ mve Readdme.md Readme.md
-
-    # Move a directory:
-    $ mve --mkdirp source/directory target/directory
 
     # Read from a file, then pipe into it:
     $ format-json package.json > .temp; mve --force .temp package.json
