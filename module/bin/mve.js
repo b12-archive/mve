25d55c1e98332896800cbf41a9e4654402d0925f
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-06 13:22:39 +0200

    Implement behavior for a single file

--- module/bin/mve.js
+++ module/bin/mve.js
@@ -2,6 +2,8 @@
 
 const {stdout, stderr, exit, argv} = process;
 
+const mv = require('mv');
+
 const flags = require('minimist')(argv.slice(2));
 const files = flags._;
 
@@ -19,3 +21,12 @@ if (files.length !== 2) {
   stderr.write(require('./help/usage'));
   exit(1);
 }
+
+mv(files[0], files[1], (error) => {
+  if (error) {
+    stderr.write(error.message + '\n');
+    exit(2);
+  }
+
+  exit(0);
+});
